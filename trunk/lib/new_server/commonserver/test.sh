74c7d718395c09a05b7d165ac403a276ab1f9606
ruqiang826@gmail.com <ruqiang826@gmail.com@290e4b4b-e8a2-e9ca-5427-c2c003840725>
2012-09-06 06:28:11 +0000

    ci

--- trunk/lib/new_server/commonserver/test.sh
+++
@@ -1,6 +0,0 @@
-if [ $# -lt 2 ]
-then
-	echo "usage:<infile> <outfile>"
-	exit 1
-fi
-time ./test localhost 20241 $1 "\n" $2 2>log2.txt
--- trunk/lib/new_server/commonserver2/test.sh
+++
@@ -1,6 +0,0 @@
-if [ $# -lt 2 ]
-then
-	echo "usage:<infile> <outfile>"
-	exit 1
-fi
-time ./commonclient localhost 23202 $1 "\n" $2 2>log2.txt
